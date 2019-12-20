e06adf91ccc66d21b1afc6282a2f0216e9bb9d61
Jeremy Bourne <jbourne25@yahoo.com>
2019-12-20 03:17:35 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -21,7 +21,7 @@ class Patient
   
   def appointments
     #will iterate through all the appointments, then returns appointments that belong to the patient 
-    Appointment.all.{ |appointment| appointment.patient }
+    Appointment.all.detect{ |appointment| appointment.patient }
   end 
   
   def doctors 
