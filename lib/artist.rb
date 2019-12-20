3261942d9ae6742121a7694d3c0cd0154d6eb0f3
Jeremy Bourne <jbourne25@yahoo.com>
2019-12-20 04:27:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -27,7 +27,7 @@ class Artist
   def genres 
     #will iterate over the artist's songs, then collects the genre of each song
     Song.all.find_all{ |song| song.genre }
-    song.genre 
+    genre 
   end 
   
 end 
